18ecc33fa1cc2249777e0821e21839f1141232d9
Omar Alomar <omaro2000@gmail.com>
2023-01-04 01:27:15 -0500

    gui, date splicer

--- gen.py
+++ gen.py
@@ -1,68 +1,44 @@
 from docxtpl import DocxTemplate
-
-projectName = ""
-addressSingleLine = ""
-sdp = ""
-taxMap = 0
-parcel = 0
-author = ""
-monthDate = ""
-peLicenseNo = 0
-peExpirationDate = ""
-peFullName = ""
-dateShort = ""
-addressShort = ""
-startTime = ""
-endTime = ""
-dateLong = ""
-weather = ""
-temp = 0.0
-totalVehs = 0
-speedLimit = 0
-roadName = ""
-direction1 = ""
-direction2 = ""
-maxSpeedDir1 = 0.0
-maxSpeedDir2 = 0.0
-minSpeedDir1 = 0.0
-minSpeedDir2 = 0.0
-noVehsDir1 = 0
-noVehsDir2 = 0
-speed85Dir1 = 0
-speed85Dir2 = 0
+import PySimpleGUI as sg
+from datetime import datetime
 
 doc = DocxTemplate('template.docx')
 
-context = { 'address_single_line': addressSingleLine,
-            'sdp': sdp,
-            'project_name': projectName,
-            'tax_map': taxMap,
-            'parcel': parcel,
-            'author': author,
-            'month_date': monthDate,
-            'pe_license_no': peLicenseNo,
-            'pe_expiration': peExpirationDate,
-            'pe_full_name': peFullName,
-            'address_short': addressShort,
-            'start_time': startTime,
-            'end_time': endTime,
-            'date_long': dateLong,
-            'weather': weather,
-            'temp': temp,
-            'total_vehs': totalVehs,
-            'speed_limit': speedLimit,
-            'road_name': roadName,
-            'direction1': direction1,
-            'direction2': direction2,
-            'max_speed_dir1': maxSpeedDir1,
-            'min_speed_dir1': minSpeedDir1,
-            'max_speed_dir2': maxSpeedDir2,
-            'min_speed_dir2': minSpeedDir2,
-            'no_vehs_dir1': noVehsDir1,
-            'no_vehs_dir2': noVehsDir2,
-            'speed_85_dir1': speed85Dir1,
-            'speed_85_dir2': speed85Dir2,
-            }
+layout = [
+    [sg.Text("Project Name"), sg.Input(key="project_name")],
+    [sg.Text("Address"), sg.Input(key="address")],
+    [sg.Text("Speed Limit"), sg.Input(key="speed_limit")],
+    [sg.Text("Tax Map"), sg.Input(key="tax_map")],
+    [sg.Text("Parcel"), sg.Input(key="parcel")],
+    [sg.Text("SDP"), sg.Input(key="sdp")],
+    [sg.Text("Author"), sg.Input(key="author")],
+    [sg.Text("P.E. Full Name"), sg.Input(key="pe_full_name")],
+    [sg.Text("P.E. License Number"), sg.Input(key="pe_license_no")],
+    [sg.Text("P.E. License Expiry Date"), sg.Input(key="pe_expiration")],
+    [sg.Text("Date"), sg.Input(key="date", size=(20,1)), sg.CalendarButton("Select", close_when_date_chosen=True, target="date")],
+    [sg.Text("Start time"), sg.Input(key="start_time")],
+    [sg.Text("End time"), sg.Input(key="end_time")],
+    [sg.Text("Weather"), sg.Input(key="weather")],
+    [sg.Text("Temperature (F)"), sg.Input(key="temp")],
+    [sg.Button("Generate Study"), sg.Exit()]
+]
+window = sg.Window("Free Flow Study Generator", layout, element_justification='right')
 
-doc.render(context)
-doc.save('output.docx')
+
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    if event == 'Generate Study':
+        print(event, values)
+
+        tempDate = datetime.strptime(values['date'], '%Y-%m-%d %H:%M:%S')
+        values['date'] = tempDate.strftime('%Y-%m-%d')
+    
+
+        doc.render(values)
+        doc.save('output.docx')
+        sg.popup("Free flow study generated successfully.")
+
+
---
+++ probably_useless.py
@@ -0,0 +1,64 @@
+
+
+context = { 'address': address,
+            'sdp': sdp,
+            # 'project_name': projectName,
+            'tax_map': taxMap,
+            'parcel': parcel,
+            'author': author,
+            'month_date': monthDate,
+            'pe_license_no': peLicenseNo,
+            'pe_expiration': peExpirationDate,
+            'pe_full_name': peFullName,
+            'address_short': addressShort,
+            'start_time': startTime,
+            'end_time': endTime,
+            'date_long': dateLong,
+            'weather': weather,
+            'temp': temp,
+            'total_vehs': totalVehs,
+            'speed_limit': speedLimit,
+            'road_name': roadName,
+            'direction1': direction1,
+            'direction2': direction2,
+            'max_speed_dir1': maxSpeedDir1,
+            'min_speed_dir1': minSpeedDir1,
+            'max_speed_dir2': maxSpeedDir2,
+            'min_speed_dir2': minSpeedDir2,
+            'no_vehs_dir1': noVehsDir1,
+            'no_vehs_dir2': noVehsDir2,
+            'speed_85_dir1': speed85Dir1,
+            'speed_85_dir2': speed85Dir2,
+            }
+
+projectName = ""
+address = ""
+sdp = ""
+taxMap = 0
+parcel = 0
+author = ""
+monthDate = "" #
+peLicenseNo = 0
+peExpirationDate = ""
+peFullName = ""
+date = ""
+dateShort = "" # auto-gen (?)
+addressShort = ""
+startTime = ""
+endTime = ""
+dateLong = ""
+weather = ""
+temp = 0.0
+totalVehs = 0
+speedLimit = 0
+roadName = ""
+direction1 = ""
+direction2 = ""
+maxSpeedDir1 = 0.0
+maxSpeedDir2 = 0.0
+minSpeedDir1 = 0.0
+minSpeedDir2 = 0.0
+noVehsDir1 = 0
+noVehsDir2 = 0
+speed85Dir1 = 0
+speed85Dir2 = 0
